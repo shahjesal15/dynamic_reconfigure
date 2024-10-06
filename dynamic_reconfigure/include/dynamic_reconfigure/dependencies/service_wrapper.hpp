ce4e666e130ae9b5a551d82d6993a6d2c1f11af1
Jesal Shah <shahjesal1510@gmail.com>
2024-10-06 18:56:30 +0530

    Added service manager function that can list the parameters and type of parameters available for the node.

---
+++ dynamic_reconfigure/include/dynamic_reconfigure/dependencies/service_wrapper.hpp
@@ -0,0 +1,68 @@
+/**
+ * @file         : service_wrapper.hpp
+ * @author       : Jesal Shah
+ * @date         : 06-10-2024
+ * @description  : Service manager is heart of dynamic reconfiguration and enables the node 
+ *                 to change or access the parameters of other nodes on the fly.
+ */
+
+#ifndef SERVICE_WRAPPER_HPP__
+#define SERVICE_WRAPPER_HPP__
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rcl_interfaces/srv/set_parameters.hpp>
+#include <rcl_interfaces/srv/get_parameters.hpp>
+#include <rcl_interfaces/srv/list_parameters.hpp>
+#include <rcl_interfaces/srv/describe_parameters.hpp>
+
+namespace reconfigure_depends
+{
+    class ServiceWrapper
+    {
+    protected:
+        /// @brief holds the information of the last accessed node
+        std::string prev_accessed_node;
+        /// @brief holds the list of the parameters of the previously accessed node
+        std::vector<std::string> parameters;
+        /// @brief holds the map for type of parameter for the given parameter
+        std::map<std::string, std::string> parameter_type;
+        /// @brief updates the parameters of the particular node
+        void update_parameters();
+        /// @brief create the client to access the services
+        void create_client(const std::string &node_name);
+        /// @brief shared pointer to node
+        rclcpp::Node::SharedPtr node_;
+        /// @brief shared pointer to client that can set parameters
+        rclcpp::Client<rcl_interfaces::srv::SetParameters>::SharedPtr set_parameters_client_;
+        /// @brief shared pointer to client that can get parameters
+        rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedPtr get_parameters_client_;
+        /// @brief shared pointer to client that can list parameters
+        rclcpp::Client<rcl_interfaces::srv::ListParameters>::SharedPtr list_parameters_client_;
+        /// @brief shared pointer to client that can describe parameters
+        rclcpp::Client<rcl_interfaces::srv::DescribeParameters>::SharedPtr describe_parameters_client_;
+    public:
+        /// @brief Constructor for the class
+        ServiceWrapper(rclcpp::Node::SharedPtr node);
+        /// @brief Returns a vector of available parameters for the given node
+        /// @param name
+        /// @return std::vector<std::string>&
+        std::vector<std::string>& list_parameters(std::string &node_name);
+        /// @brief This function sets the value of the given parameter
+        /// @param name
+        /// @param value
+        /// @return bool
+        bool set_param(const std::string &name, const rclcpp::ParameterValue &value);
+        /// @brief This function gets the value of the given parameter
+        /// @param name
+        /// @return rclcpp::ParameterValue
+        rclcpp::ParameterValue get_param(const std::string &name);
+        /// @brief Destructor for the class
+        ~ServiceWrapper() {}
+    };
+};
+
+#endif // SERVICE_WRAPPER_HPP__
