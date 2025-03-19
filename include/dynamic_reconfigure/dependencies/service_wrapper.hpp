4acfc7920d0b38dd529f51a7096c6b29448c52db
shahjesal15 <shahjesal1510@gmail.com>
2025-03-19 18:11:09 +0530

    Issue #1 added features to get and list parameters.

---
+++ include/dynamic_reconfigure/dependencies/service_wrapper.hpp
@@ -0,0 +1,158 @@
+/**
+ * @file         : service_wrapper.hpp
+ * @author       : Jesal Shah
+ * @date         : 19-03-24
+ * @brief        : The Service Manager serves as the core of dynamic reconfiguration,
+ *                 allowing nodes to modify or access the parameters of other nodes in real time.
+ */
+
+#ifndef SERVICE_WRAPPER_HPP__
+#define SERVICE_WRAPPER_HPP__
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <atomic>
+#include <mutex>
+#include <algorithm>
+#include <fmt/core.h>
+#include <fmt/color.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rcl_interfaces/srv/set_parameters.hpp>
+#include <rcl_interfaces/srv/get_parameters.hpp>
+#include <rcl_interfaces/srv/list_parameters.hpp>
+#include <rcl_interfaces/srv/describe_parameters.hpp>
+
+namespace dynamic_reconfigure_core
+{
+    enum ServiceWrapperReturnCodes
+    {
+        FAILURE = -1,
+        SUCCESS = 0,
+        BUSY
+    };
+
+    enum ServiceWrapperStates
+    {
+        IDLE = 0,
+        PROCESSING,
+        COMPLETE,
+        ERROR
+    };
+
+    using AtomicServiceWrapperState = std::atomic<ServiceWrapperStates>;
+
+    class ServiceWrapper
+    {
+    public:
+        /// @brief Constructor for the ServiceWrapper class
+        /// @param node
+        ServiceWrapper(rclcpp::Node::SharedPtr node);
+
+        /// @brief lists the available parameters of the node.
+        /// @param node_name
+        /// @return ServiceWrapperReturnCodes
+        ServiceWrapperReturnCodes request_params_list(std::string node_name);
+
+        /// @brief sets the parameters for the selected node
+        /// @param params
+        /// @return ServiceWrapperReturnCodes
+        ServiceWrapperReturnCodes set_params(const std::vector<rclcpp::Parameter> &params);
+
+        /// @brief get the parameters for the selected node
+        /// @param param_names
+        /// @return ServiceWrapperReturnCodes
+        ServiceWrapperReturnCodes request_params(const std::vector<std::string> &param_names);
+
+        /// @brief get the status of list retrieval co-routine
+        /// @return ServiceWrapperStates
+        ServiceWrapperStates get_list_status();
+
+        /// @brief get the status of the request params co-routine
+        /// @return ServiceWrapperStates
+        ServiceWrapperStates get_request_status();
+
+        /// @brief get params list request before
+        /// @return std::vector<std::string>
+        std::vector<std::string> get_params_list();
+
+        /// @brief get the requested params
+        /// @return std::map<std::string, rcl_interfaces::msg::ParameterValue>
+        std::map<std::string, rcl_interfaces::msg::ParameterValue> get_params();
+
+        /// @brief clears all the error states
+        void clear_errors();
+
+        /// @brief reset all the states
+        void reset_states();
+
+    protected:
+        /// @brief shared pointer to node
+        rclcpp::Node::SharedPtr node_;
+
+        /// @brief list params status state variable.
+        AtomicServiceWrapperState list_params_status;
+
+        /// @brief request params status state variable
+        AtomicServiceWrapperState request_params_status;
+
+        /// @brief mutex to handle clients related resource sharing
+        std::mutex client_mutex;
+
+        /// @brief mutex to handle params related resource sharing
+        std::mutex params_mutex;
+
+        /// @brief mutex to handle request params resource sharing
+        std::mutex requested_params_mutex;
+
+        /// @brief parameters listed from the node
+        std::vector<std::string> params;
+
+        /// @brief parameters that are request from the node
+        std::vector<std::string> requested_params;
+
+        std::map<std::string, rcl_interfaces::msg::ParameterValue> retrieved_params;
+
+        /// @brief holds the map for type of parameter for the given parameter
+        std::map<std::string, int> parameter_types;
+
+        /// @brief shared pointer to client that can set params
+        rclcpp::Client<rcl_interfaces::srv::SetParametersAtomically>::SharedPtr set_params_client_;
+
+        /// @brief shared pointer to client that can get params
+        rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedPtr get_params_client_;
+
+        /// @brief shared pointer to client that can list params
+        rclcpp::Client<rcl_interfaces::srv::ListParameters>::SharedPtr list_params_client_;
+
+        /// @brief shared pointer to client that can describe params
+        rclcpp::Client<rcl_interfaces::srv::DescribeParameters>::SharedPtr describe_params_client_;
+
+        /// @brief creates the client to set, get or list params and it's types.
+        /// @param node_name
+        void create_client(const std::string node_name);
+
+        /// @brief list the parameter types of the selected node
+        void list_parameter_types();
+
+    private:
+        /// @brief this function is the callback for the list_params service call
+        /// @param future
+        void list_params_cb(const rclcpp::Client<rcl_interfaces::srv::ListParameters>::SharedFuture future);
+
+        /// @brief this function is the callback for the list_param_types service call
+        /// @param future
+        void list_param_types_cb(const rclcpp::Client<rcl_interfaces::srv::DescribeParameters>::SharedFuture future);
+
+        /// @brief this function is the callback for the set_params service call
+        /// @param future
+        void set_params_cb(const rclcpp::Client<rcl_interfaces::srv::SetParametersAtomically>::SharedFuture future);
+
+        /// @brief this function is the callback for the get_params service call
+        /// @param future
+        void request_params_cb(const rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture future);
+    };
+};
+
+#endif // SERVICE_WRAPPER_HPP__
---
+++ src/dependencies/service_wrapper.cpp
@@ -0,0 +1,242 @@
+#include "dependencies/service_wrapper.hpp"
+
+namespace dynamic_reconfigure_core
+{
+    ServiceWrapper::ServiceWrapper(rclcpp::Node::SharedPtr node) : node_(node)
+    {
+        list_params_status.store(ServiceWrapperStates::IDLE);
+        request_params_status.store(ServiceWrapperStates::IDLE);
+        params.clear();
+        parameter_types.clear();
+    }
+
+    ServiceWrapperReturnCodes ServiceWrapper::request_params_list(std::string node_name)
+    {
+        if (list_params_status.load() != ServiceWrapperStates::IDLE)
+            return ServiceWrapperReturnCodes::BUSY;
+
+        RCLCPP_DEBUG_STREAM(node_->get_logger(),
+                            fmt::format(fg(fmt::color::blue), "requesting list parameters from {}", node_name));
+
+        create_client(node_name);
+
+        list_params_status.store(ServiceWrapperStates::PROCESSING);
+
+        auto list_request = std::make_shared<rcl_interfaces::srv::ListParameters::Request>();
+        list_request->depth = rcl_interfaces::srv::ListParameters::Request::DEPTH_RECURSIVE;
+
+        auto list_future = list_params_client_->async_send_request(list_request,
+                                                                   std::bind(&ServiceWrapper::list_params_cb, this, std::placeholders::_1));
+
+        return ServiceWrapperReturnCodes::SUCCESS;
+    }
+
+    ServiceWrapperReturnCodes ServiceWrapper::request_params(const std::vector<std::string> &param_names)
+    {
+        if (request_params_status.load() != ServiceWrapperStates::IDLE)
+            return ServiceWrapperReturnCodes::BUSY;
+
+        RCLCPP_DEBUG_STREAM(node_->get_logger(),
+                            fmt::format(fg(fmt::color::blue), "requesting parameter values"));
+
+        requested_params_mutex.lock();
+        requested_params.clear();
+        retrieved_params.clear();
+
+        auto get_param_request = std::make_shared<rcl_interfaces::srv::GetParameters::Request>();
+
+        rcl_interfaces::msg::ParameterValue temp_value;
+
+        for (std::string name : param_names)
+        {
+            if (std::find(params.begin(), params.end(), name) == params.end())
+            {
+                RCLCPP_WARN_STREAM(node_->get_logger(),
+                                   fmt::format(fg(fmt::color::yellow), "Trying to get no-existent parameter, skipping {}", name));
+                continue;
+            }
+            else
+            {
+                RCLCPP_DEBUG_STREAM(node_->get_logger(),
+                                    fmt::format(fg(fmt::color::blue), " [@] Parameter found and trying to access {}", name));
+            }
+            retrieved_params[name] = temp_value;
+            requested_params.push_back(name);
+            get_param_request->names.push_back(name);
+        }
+
+        requested_params_mutex.unlock();
+
+        std::lock_guard client_lock(client_mutex);
+
+        request_params_status.store(ServiceWrapperStates::PROCESSING);
+
+        auto get_param_future = get_params_client_->async_send_request(
+            get_param_request,
+            std::bind(&ServiceWrapper::request_params_cb, this, std::placeholders::_1));
+
+        return ServiceWrapperReturnCodes::SUCCESS;
+    }
+
+    ServiceWrapperReturnCodes ServiceWrapper::set_params(const std::vector<rclcpp::Parameter> &params)
+    {
+    }
+
+    void ServiceWrapper::list_parameter_types()
+    {
+        std::lock_guard client_lock(client_mutex);
+
+        auto type_request = std::make_shared<rcl_interfaces::srv::DescribeParameters::Request>();
+
+        params_mutex.lock();
+        for (std::string &param : params)
+            type_request->names.push_back(param);
+        params_mutex.unlock();
+
+        auto list_future = describe_params_client_->async_send_request(type_request,
+                                                                       std::bind(&ServiceWrapper::list_param_types_cb, this, std::placeholders::_1));
+    }
+
+    std::vector<std::string> ServiceWrapper::get_params_list()
+    {
+        std::lock_guard params_lock(params_mutex);
+
+        if (list_params_status.load() != ServiceWrapperStates::ERROR)
+            list_params_status.store(ServiceWrapperStates::IDLE);
+
+        return params;
+    }
+
+    std::map<std::string, rcl_interfaces::msg::ParameterValue> ServiceWrapper::get_params()
+    {
+        std::lock_guard requested_params_lock(requested_params_mutex);
+
+        if (request_params_status.load() != ServiceWrapperStates::ERROR)
+            request_params_status.store(ServiceWrapperStates::IDLE);
+
+        return retrieved_params;
+    }
+
+    ServiceWrapperStates ServiceWrapper::get_list_status()
+    {
+        return list_params_status.load();
+    }
+
+    ServiceWrapperStates ServiceWrapper::get_request_status()
+    {
+        return request_params_status.load();
+    }
+
+    void ServiceWrapper::create_client(const std::string node_name)
+    {
+        std::lock_guard client_lock(client_mutex);
+
+        set_params_client_ = node_->create_client<rcl_interfaces::srv::SetParametersAtomically>("/" + node_name + "/set_parameters_atomically");
+        get_params_client_ = node_->create_client<rcl_interfaces::srv::GetParameters>("/" + node_name + "/get_parameters");
+        list_params_client_ = node_->create_client<rcl_interfaces::srv::ListParameters>("/" + node_name + "/list_parameters");
+        describe_params_client_ = node_->create_client<rcl_interfaces::srv::DescribeParameters>("/" + node_name + "/describe_parameters");
+    }
+
+    void ServiceWrapper::clear_errors()
+    {
+        RCLCPP_DEBUG_STREAM(node_->get_logger(),
+                            fmt::format(fg(fmt::color::turquoise), "clearing errors"));
+        if (list_params_status.load() == ServiceWrapperStates::ERROR)
+            list_params_status.store(ServiceWrapperStates::IDLE);
+    }
+
+    void ServiceWrapper::list_params_cb(
+        const rclcpp::Client<rcl_interfaces::srv::ListParameters>::SharedFuture future)
+    {
+        auto result = future.get();
+        if (!result)
+        {
+            RCLCPP_ERROR_STREAM(node_->get_logger(),
+                                fmt::format(fg(fmt::color::red), "Failed to retrieve list of parameters."));
+            list_params_status.store(ServiceWrapperStates::ERROR);
+            return;
+        }
+
+        params_mutex.lock();
+        params.clear();
+
+        for (auto &name : result->result.names)
+        {
+            RCLCPP_DEBUG_STREAM(node_->get_logger(),
+                                fmt::format(fg(fmt::color::blue), " [@] Parameter : {}", name));
+            params.push_back(name);
+        }
+
+        params_mutex.unlock();
+
+        list_parameter_types();
+    }
+
+    void ServiceWrapper::list_param_types_cb(
+        const rclcpp::Client<rcl_interfaces::srv::DescribeParameters>::SharedFuture future)
+    {
+        auto result = future.get();
+        if (!result)
+        {
+            RCLCPP_ERROR_STREAM(node_->get_logger(),
+                                fmt::format(fg(fmt::color::blue), "Failed to retrieve list of parameters."));
+            list_params_status.store(ServiceWrapperStates::ERROR);
+            return;
+        }
+
+        params_mutex.lock();
+        parameter_types.clear();
+
+        for (auto &descriptor : result->descriptors)
+        {
+            parameter_types[descriptor.name] = descriptor.type;
+            RCLCPP_DEBUG_STREAM(node_->get_logger(), fmt::format(fg(fmt::color::blue),
+                                                                 " [@] Parameter : {}\tType : {}", descriptor.name, parameter_types[descriptor.name]));
+        }
+        params_mutex.unlock();
+
+        list_params_status.store(ServiceWrapperStates::COMPLETE);
+    }
+
+    void ServiceWrapper::request_params_cb(const rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture future)
+    {
+        auto result = future.get();
+
+        uint32_t not_set_params = 0;
+
+        if (!result)
+        {
+            request_params_status.store(ServiceWrapperStates::ERROR);
+            RCLCPP_WARN_STREAM(node_->get_logger(), fmt::format(
+                                                        fg(fmt::color::yellow), "parameter get failure, with unknown error"));
+            return;
+        }
+
+        requested_params_mutex.lock();
+
+        if (result->values.size() != requested_params.size())
+        {
+            request_params_status.store(ServiceWrapperStates::ERROR);
+            RCLCPP_WARN_STREAM(node_->get_logger(), fmt::format(
+                                                        fg(fmt::color::yellow), "parameter get failure, didn't receive the number of requested parameters"));
+            return;
+        }
+
+        for (uint8_t index = 0; index < result->values.size(); index++)
+        {
+            if (result->values[index].type == rclcpp::ParameterType::PARAMETER_NOT_SET)
+                not_set_params++;
+            retrieved_params[requested_params[index]] = result->values[index];
+        }
+
+        requested_params_mutex.unlock();
+
+        if (not_set_params > 0)
+        {
+            RCLCPP_WARN_STREAM(node_->get_logger(),
+                               fmt::format(fg(fmt::color::blue), "some of the parameters are not set yet."));
+        }
+
+        request_params_status.store(ServiceWrapperStates::COMPLETE);
+    }
+};
